01817adc2eff45482d6164a5901bbfaf3272d930
marcosguilhermefh@gmail.com <marcosguilhermefh@gmail.com>
2021-05-14 02:00:23 -0300

    first commit

---
+++ resources/js/Pages/Inflacao.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+import Layout from '../Shared/Layouts'
+
+class Inflacao extends React.Component  {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return <Layout> <h1> Aparentemente tudo funciona do jeito que acho que deveria! </h1> </Layout>
+    }
+}
+
+export default Inflacao
---
+++ resources/js/Pages/Inflacao1.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import Layout from '../Shared/Layouts'
+
+export class Inflacao extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return <Layout> <h1> Aparentemente tudo funciona do jeito que acho que deveria! </h1> </Layout>
+        
+    }
+}
